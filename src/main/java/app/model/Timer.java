5b8e01dd5bc36ce1a6a2ac92efec9b970535db8a
Vino9 <vino9@mail.ru>
2022-10-05 21:57:29 +0700

    Done

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,22 @@
 package app.model;
 
+
+import org.springframework.stereotype.Component;
+
+@Component("timer")
 public class Timer {
+    @Override
+    public String toString() {
+        return "Timer{" +
+                "nanoTime=" + nanoTime +
+                '}';
+    }
+
+
+
+    public void setNanoTime(Long nanoTime) {
+        this.nanoTime = nanoTime;
+    }
 
     private Long nanoTime = System.nanoTime();
 
